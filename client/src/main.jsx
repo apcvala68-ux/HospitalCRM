646d7cd0d95d8bd6266da9d8f7db4399b9e43dcc
MayurPatel720 <mayurpatell7020@gmail.com>
2026-05-19 02:46:23 +0530

    fix: remove redundant HeroUIProvider import since HeroUI v3 is zero-boilerplate and provider-less

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'sonner';
 import { ThemeProvider } from './context/ThemeContext';
-import { HeroUIProvider } from '@heroui/react';
 import App from './App.jsx';
 import '@fontsource-variable/inter';
 import './index.css';
@@ -22,12 +21,10 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <HeroUIProvider>
-        <ThemeProvider>
-          <App />
-          <Toaster richColors position="top-right" closeButton />
-        </ThemeProvider>
-      </HeroUIProvider>
+      <ThemeProvider>
+        <App />
+        <Toaster richColors position="top-right" closeButton />
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
